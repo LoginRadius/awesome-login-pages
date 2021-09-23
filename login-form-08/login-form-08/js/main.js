bdfe0210b1be430d75721f61b77537d3cd7e1c2f
Hiteshkrkumawat <hiteshkumawat19@gmail.com>
2021-09-23 15:24:11 +0530

    themes screenshot added

--- login-form-08/login-form-08/js/main.js
+++ login-form-08/login-form-08/js/main.js
@@ -1,14 +1,6 @@
-$(function() {
-	'use strict';
+(function($) {
 
-	
-  $('.form-control').on('input', function() {
-	  var $field = $(this).closest('.form-group');
-	  if (this.value) {
-	    $field.addClass('field--not-empty');
-	  } else {
-	    $field.removeClass('field--not-empty');
-	  }
-	});
+	"use strict";
 
-});
+
+})(jQuery);
