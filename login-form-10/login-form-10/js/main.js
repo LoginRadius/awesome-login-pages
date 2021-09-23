47bb6033c414e60d979efd0bf9d5b3ec923c2c7e
Hiteshkrkumawat <32321020+Hiteshkrkumawat@users.noreply.github.com>
2021-09-23 16:41:28 +0530

    themes screenshot added (#3)
    Co-authored-by: Hiteshkrkumawat <hiteshkumawat19@gmail.com>

--- login-form-10/login-form-10/js/main.js
+++ login-form-10/login-form-10/js/main.js
@@ -1,14 +1,16 @@
-$(function() {
-	'use strict';
+(function($) {
 
-	
-  $('.form-control').on('input', function() {
-	  var $field = $(this).closest('.form-group');
-	  if (this.value) {
-	    $field.addClass('field--not-empty');
-	  } else {
-	    $field.removeClass('field--not-empty');
-	  }
-	});
+	"use strict";
 
+	$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
 });
+
+})(jQuery);
