3ab146bc1e24c1c807b0b0e05f65f46a33f67168
Versha <versha.gupta@ad.loginradius.net>
2021-10-27 17:21:15 +0530

    Added new animated login form theme

---
+++ login-form-v35/script.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body");
+const modal = document.querySelector(".modal");
+const modalButton = document.querySelector(".modal-button");
+const closeButton = document.querySelector(".close-button");
+const scrollDown = document.querySelector(".scroll-down");
+let isOpened = false;
+
+const openModal = () => {
+  modal.classList.add("is-open");
+  body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+  modal.classList.remove("is-open");
+  body.style.overflow = "initial";
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > window.innerHeight / 3 && !isOpened) {
+    isOpened = true;
+    scrollDown.style.display = "none";
+    openModal();
+  }
+});
+
+modalButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+
+document.onkeydown = (evt) => {
+  evt = evt || window.event;
+  evt.keyCode === 27 ? closeModal() : false;
+};
